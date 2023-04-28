678e4c0f40cbfaf1b443fef10da836095d87ce17
John <jhnjacob300@gmail.com>
2023-04-28 17:52:27 +0530

    Sample projrct commit

---
+++ src/sample/Employee.java
@@ -0,0 +1,47 @@
+package sample;
+
+public class Employee {
+	
+	private String name ;
+	private int  id ;
+	private  int salary;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getAlary() {
+		return salary;
+	}
+
+
+	public void setAlary(int alary) {
+		this.salary = salary;
+	}
+
+
+	public Employee(String name ,int id ,int salary )
+	{
+		
+
+		this.name
+	}
+
+}
---
+++ src/sample/Sort.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.*;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	List<Employee> emp = Array.asList(
+			new Employee ("John",25,690000),
+		new	Employee ("Ram",25,600000),
+		new	Employee ("Sham",32,750000),
+		new	Employee ("John",28,450000));
+	
+	
+	emp.stream().sorted(x1,x2) -> x1.getName().compare
+
+	}
+
+}
