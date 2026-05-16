228cd0971bcc4b7d9f9f6cd5fb0a454edfaca615
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-16 17:39:14 +0530

    Create RegisterOcasInsertCCDBReturn.java

---
+++ SBI/src/com/mintstreet/callback/service/RegisterOcasInsertCCDBReturn.java
@@ -0,0 +1,11 @@
+package com.mintstreet.callback.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "register_OCAS_INSERT_CCDBReturn")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RegisterOcasInsertCCDBReturn {
+
+}
