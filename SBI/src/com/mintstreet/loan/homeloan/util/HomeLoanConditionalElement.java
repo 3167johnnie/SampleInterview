99deb6f17710f9a1f26bdfa46f666fb6fbe48d41
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-16 17:00:24 +0530

    Create HomeLoanConditionalElement.java

---
+++ SBI/src/com/mintstreet/loan/homeloan/util/HomeLoanConditionalElement.java
@@ -0,0 +1,7 @@
+package com.mintstreet.loan.homeloan.util;
+
+public class HomeLoanConditionalElement {
+	
+	public static String  [] homeLoanempValidationType={"loanPurpose","employmentType","residentType","co_employmentType1","co_employmentType2","co_applicant_relationship1"};
+
+}
