bac6aa3542886d5fc8e4a30364ce1a319a3df871
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-16 16:51:53 +0530

    Create ApplicationFormHomeLoanCallsDao.java

---
+++ SBI/src/com/mintstreet/loan/homeloan/dao/ApplicationFormHomeLoanCallsDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.loan.homeloan.dao;
+
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.homeloan.entity.ApplicationFormHomeLoanCalls;
+
+public class ApplicationFormHomeLoanCallsDao extends GenericDao<Integer, ApplicationFormHomeLoanCalls> {
+	private static final long serialVersionUID = -4095972777166139972L;
+}
