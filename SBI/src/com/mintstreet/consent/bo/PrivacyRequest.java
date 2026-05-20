cb993609940a3887dc11fe5d4f50870e930c9aed
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-20 12:24:10 +0530

    Create PrivacyRequest.java

---
+++ SBI/src/com/mintstreet/consent/bo/PrivacyRequest.java
@@ -0,0 +1,53 @@
+package com.mintstreet.consent.bo;
+
+//120526 changes for locale null issue
+public class PrivacyRequest {
+	
+	private String touchPointId;
+	private String privacyNotice;
+	private String locale;
+	private Integer version;
+	private String timestamp;
+	private String correlationId;
+	
+	public String getTouchPointId() {
+		return touchPointId;
+	}
+	public void setTouchPointId(String touchPointId) {
+		this.touchPointId = touchPointId;
+	}
+	public String getPrivacyNotice() {
+		return privacyNotice;
+	}
+	public void setPrivacyNotice(String privacyNotice) {
+		this.privacyNotice = privacyNotice;
+	}
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getCorrelationId() {
+		return correlationId;
+	}
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+	
+	
+	
+
+}
