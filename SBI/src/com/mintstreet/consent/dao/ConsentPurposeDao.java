a1c7a22776014b91a6968fec400e16348dc44e3a
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-20 12:31:21 +0530

    Create ConsentPurposeDao.java

---
+++ SBI/src/com/mintstreet/consent/dao/ConsentPurposeDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.consent.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.consent.entity.ConsentPurposeLog;
+
+public class ConsentPurposeDao extends GenericDao<Integer, ConsentPurposeLog>{
+	private static final long serialVersionUID = 404346123313078900L;
+
+}
