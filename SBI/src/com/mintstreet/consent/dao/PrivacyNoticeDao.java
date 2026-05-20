a712414f80c4c4f569ba2e213f073c67a4259b54
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-20 12:47:06 +0530

    Create PrivacyNoticeDao.java

---
+++ SBI/src/com/mintstreet/consent/dao/PrivacyNoticeDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.consent.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.consent.entity.PrivacyRequestResponse;
+
+public class PrivacyNoticeDao extends GenericDao<Integer, PrivacyRequestResponse> { 
+	private static final long serialVersionUID = 1L;
+
+}
