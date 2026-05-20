bef459fe53280c4410b455baf79a7676cf6145d6
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-20 10:54:33 +0530

    Create ContentDao.java

---
+++ SBI/src/com/mintstreet/common/dao/ContentDao.java
@@ -0,0 +1,8 @@
+package com.mintstreet.common.dao;
+
+import com.mintstreet.common.entity.MasterContent;
+
+public class ContentDao extends GenericDao<Integer, MasterContent> {
+	private static final long serialVersionUID = -8289309507835676247L;
+
+}
