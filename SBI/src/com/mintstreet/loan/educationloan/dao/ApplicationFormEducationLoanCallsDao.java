5968126be5219dcb85da0e0a6d29df7a5be99108
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-30 10:59:10 +0530

    Create ApplicationFormEducationLoanCallsDao.java

---
+++ SBI/src/com/mintstreet/loan/educationloan/dao/ApplicationFormEducationLoanCallsDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.loan.educationloan.dao;
+
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.educationloan.entity.ApplicationFormEducationLoanCalls;
+
+public class ApplicationFormEducationLoanCallsDao extends GenericDao<Integer, ApplicationFormEducationLoanCalls> {
+	private static final long serialVersionUID = -4095972777166139972L;
+}
