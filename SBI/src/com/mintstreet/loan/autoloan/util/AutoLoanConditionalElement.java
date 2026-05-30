7d34e3c68b86ff473b569d0ab6ccab5212a79ff3
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-30 12:11:01 +0530

    Create AutoLoanConditionalElement.java

---
+++ SBI/src/com/mintstreet/loan/autoloan/util/AutoLoanConditionalElement.java
@@ -0,0 +1,7 @@
+package com.mintstreet.loan.autoloan.util;
+
+public class AutoLoanConditionalElement {
+	
+	public static String  [] autoLoanempValidationType={"loanPurpose","employmentType","residentType","co_employmentType1","co_employmentType2","co_applicant_relationship1"};
+
+}
