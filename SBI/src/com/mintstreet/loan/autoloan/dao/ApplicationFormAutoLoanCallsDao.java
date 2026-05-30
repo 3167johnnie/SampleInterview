c8628475d6d58b11dab9c2e77103e4bfd78f45a0
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-30 11:48:53 +0530

    Create ApplicationFormAutoLoanCallsDao.java

---
+++ SBI/src/com/mintstreet/loan/autoloan/dao/ApplicationFormAutoLoanCallsDao.java
@@ -0,0 +1,8 @@
+package com.mintstreet.loan.autoloan.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.autoloan.entity.ApplicationFormAutoLoanCalls;
+
+public class ApplicationFormAutoLoanCallsDao extends GenericDao<Integer, ApplicationFormAutoLoanCalls> {
+	private static final long serialVersionUID = -4095972777166139972L;
+}
