7ef1c58e30f8e9d0a2f3ddcfb101cf42bae2d546
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-05-30 11:51:50 +0530

    Create BikeTypeDao.java

---
+++ SBI/src/com/mintstreet/loan/autoloan/dao/BikeTypeDao.java
@@ -0,0 +1,8 @@
+package com.mintstreet.loan.autoloan.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.autoloan.entity.MasterBikeType;
+
+public class BikeTypeDao extends GenericDao<Integer,MasterBikeType> {
+	private static final long serialVersionUID = 1956612625003012579L;
+}
