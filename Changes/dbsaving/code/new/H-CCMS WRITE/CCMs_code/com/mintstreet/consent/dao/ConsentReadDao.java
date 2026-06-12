48ebb807d5ce575688dde1da826b306c1297fb76
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-12 19:39:59 +0530

    Update ConsentReadDao.java

--- Changes/dbsaving/code/new/H-CCMS WRITE/CCMs_code/com/mintstreet/consent/dao/ConsentReadDao.java
+++ Changes/dbsaving/code/new/H-CCMS WRITE/CCMs_code/com/mintstreet/consent/dao/ConsentReadDao.java
@@ -1,1 +1,13 @@
 
+package com.mintstreet.consent.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.consent.entity.ConsentReadLog;
+
+
+public class ConsentReadDao extends GenericDao<Integer, ConsentReadLog>{
+	private static final long serialVersionUID = 404346123313078900L;
+
+}
+
+
