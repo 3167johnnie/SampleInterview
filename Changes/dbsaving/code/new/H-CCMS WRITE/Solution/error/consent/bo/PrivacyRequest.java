d59e5c5296a18e34db14bb86fda9d0ad47580b65
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-16 20:48:38 +0530

    Create PrivacyRequest.java

---
+++ Changes/dbsaving/code/new/H-CCMS WRITE/Solution/error/consent/bo/PrivacyRequest.java
@@ -0,0 +1,27 @@
+package com.mintstreet.consent.bo;
+
+public class PrivacyRequest {
+	
+	String auth_Key;
+	String request;
+
+    
+	
+	public String getAuth_Key() {
+		return auth_Key;
+	}
+
+	public void setAuth_Key(String auth_Key) {
+		this.auth_Key = auth_Key;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	
+	
+}
