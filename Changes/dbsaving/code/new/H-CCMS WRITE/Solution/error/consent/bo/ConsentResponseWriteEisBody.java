2159f32a40d5bfcbde995c4527223aa7acde5825
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-16 20:44:13 +0530

    Create ConsentResponseWriteEisBody.java

---
+++ Changes/dbsaving/code/new/H-CCMS WRITE/Solution/error/consent/bo/ConsentResponseWriteEisBody.java
@@ -0,0 +1,51 @@
+package com.mintstreet.consent.bo;
+
+public class ConsentResponseWriteEisBody {
+	
+	private String dpCIF;
+	private String dpMobile;
+	private String dpEmail;
+	private String ntbId;
+	private String consentId;
+	
+	public String getDpCIF() {
+		return dpCIF;
+	}
+	
+	public void setDpCIF(String dpCIF) {
+		this.dpCIF = dpCIF;
+	}
+	
+	public String getDpMobile() {
+		return dpMobile;
+	}
+	
+	public void setDpMobile(String dpMobile) {
+		this.dpMobile = dpMobile;
+	}
+	
+	public String getDpEmail() {
+		return dpEmail;
+	}
+	
+	public void setDpEmail(String dpEmail) {
+		this.dpEmail = dpEmail;
+	}
+	
+	public String getNtbId() {
+		return ntbId;
+	}
+	
+	public void setNtbId(String ntbId) {
+		this.ntbId = ntbId;
+	}
+	
+	public String getConsentId() {
+		return consentId;
+	}
+	
+	public void setConsentId(String consentId) {
+		this.consentId = consentId;
+	}
+
+}
