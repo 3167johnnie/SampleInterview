1afdb2b0a58e280c978e6fa23f2e6928041936fa
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-18 22:48:48 +0530

    Create SignzyUser.java

---
+++ SBI/src/com/mintstreet/loan/personal/entity/SignzyUser.java
@@ -0,0 +1,36 @@
+package com.mintstreet.loan.personal.entity;
+
+import java.io.Serializable;
+
+public class SignzyUser implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String ttl;
+	private String created;
+	private String userId;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTtl() {
+		return ttl;
+	}
+	public void setTtl(String ttl) {
+		this.ttl = ttl;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}		
+}
