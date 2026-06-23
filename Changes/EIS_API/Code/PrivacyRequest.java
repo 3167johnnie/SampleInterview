4a96b50d6e6b567ae54d4553460fda63b025b837
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-23 22:20:11 +0530

    Create PrivacyRequest.java

---
+++ Changes/EIS_API/Code/PrivacyRequest.java
@@ -0,0 +1,28 @@
+package com.mintstreet.consent.bo;
+
+public class PrivacyRequest {
+	
+	private String REQUEST_REFERENCE_NUMBER;
+	private String REQUEST;
+	private String DIGI_SIGN;
+	
+	public String getREQUEST_REFERENCE_NUMBER() {
+		return REQUEST_REFERENCE_NUMBER;
+	}
+	public void setREQUEST_REFERENCE_NUMBER(String rEQUEST_REFERENCE_NUMBER) {
+		REQUEST_REFERENCE_NUMBER = rEQUEST_REFERENCE_NUMBER;
+	}
+	public String getREQUEST() {
+		return REQUEST;
+	}
+	public void setREQUEST(String rEQUEST) {
+		REQUEST = rEQUEST;
+	}
+	public String getDIGI_SIGN() {
+		return DIGI_SIGN;
+	}
+	public void setDIGI_SIGN(String dIGI_SIGN) {
+		DIGI_SIGN = dIGI_SIGN;
+	}
+	
+}
