dfca8aeea3f2ecf1344176d28ee3c3df11f85790
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-23 22:21:16 +0530

    Create PrivacyResponseEIS.java

---
+++ Changes/EIS_API/Code/PrivacyResponseEIS.java
@@ -0,0 +1,35 @@
+package com.mintstreet.consent.bo;
+
+public class PrivacyResponseEIS {
+	
+	private String RESPONSE;
+	private String REQUEST_REFERENCE_NUMBER;
+	private String RESPONSE_DATE;
+	private String DIGI_SIGN;
+	
+	public String getRESPONSE() {
+		return RESPONSE;
+	}
+	public void setRESPONSE(String rESPONSE) {
+		RESPONSE = rESPONSE;
+	}
+	public String getREQUEST_REFERENCE_NUMBER() {
+		return REQUEST_REFERENCE_NUMBER;
+	}
+	public void setREQUEST_REFERENCE_NUMBER(String rEQUEST_REFERENCE_NUMBER) {
+		REQUEST_REFERENCE_NUMBER = rEQUEST_REFERENCE_NUMBER;
+	}
+	public String getRESPONSE_DATE() {
+		return RESPONSE_DATE;
+	}
+	public void setRESPONSE_DATE(String rESPONSE_DATE) {
+		RESPONSE_DATE = rESPONSE_DATE;
+	}
+	public String getDIGI_SIGN() {
+		return DIGI_SIGN;
+	}
+	public void setDIGI_SIGN(String dIGI_SIGN) {
+		DIGI_SIGN = dIGI_SIGN;
+	}
+	
+}
