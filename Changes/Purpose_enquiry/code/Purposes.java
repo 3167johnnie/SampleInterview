3420013a6670621c830d2dce1d4fc3c4b858393a
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-25 17:15:31 +0530

    Create Purposes.java

---
+++ Changes/Purpose_enquiry/code/Purposes.java
@@ -0,0 +1,87 @@
+package com.mintstreet.consent.bo;
+
+public class Purposes {
+	
+	private String bankProducts;
+	private String code;
+	private String consentInfo;
+	private String dataPoints;
+	private String description;
+	private boolean isEssential;
+	private boolean isETB;
+	private boolean isProductSpecific;
+	private String title;
+	private String validityDays;
+	private String version;
+	
+	public String getBankProducts() {
+		return bankProducts;
+	}
+	public void setBankProducts(String bankProducts) {
+		this.bankProducts = bankProducts;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getConsentInfo() {
+		return consentInfo;
+	}
+	public void setConsentInfo(String consentInfo) {
+		this.consentInfo = consentInfo;
+	}
+	public String getDataPoints() {
+		return dataPoints;
+	}
+	public void setDataPoints(String dataPoints) {
+		this.dataPoints = dataPoints;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isEssential() {
+		return isEssential;
+	}
+	public void setEssential(boolean isEssential) {
+		this.isEssential = isEssential;
+	}
+	public boolean isETB() {
+		return isETB;
+	}
+	public void setETB(boolean isETB) {
+		this.isETB = isETB;
+	}
+	public boolean isProductSpecific() {
+		return isProductSpecific;
+	}
+	public void setProductSpecific(boolean isProductSpecific) {
+		this.isProductSpecific = isProductSpecific;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getValidityDays() {
+		return validityDays;
+	}
+	public void setValidityDays(String validityDays) {
+		this.validityDays = validityDays;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
+	
+
+}
