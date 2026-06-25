bc0a511ef91b58329e4bd450609bb20f6cd1cee2
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-25 21:59:31 +0530

    Create ConsentReadResponseReadEisResponse.java

---
+++ Changes/Purpose_enquiry/new/com.mintstreet.consent.bo/ConsentReadResponseReadEisResponse.java
@@ -0,0 +1,69 @@
+package com.mintstreet.consent.bo;
+
+import java.util.List;
+
+public class ConsentReadResponseReadEisResponse {
+	
+	private Body body;
+	private String correlationId;
+	private String errors;
+	private String locale;
+	private String messages;
+	private Integer statusCode;
+	private String success;
+	private String timestamp;
+	
+	public Body getBody() {
+		return body;
+	}
+	public void setBody(Body body) {
+		this.body = body;
+	}
+	public String getCorrelationId() {
+		return correlationId;
+	}
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+	public String getErrors() {
+		return errors;
+	}
+	public void setErrors(String errors) {
+		this.errors = errors;
+	}
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	public String getMessages() {
+		return messages;
+	}
+	public void setMessages(String messages) {
+		this.messages = messages;
+	}
+	public Integer getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(Integer statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+	
+	
+
+}
