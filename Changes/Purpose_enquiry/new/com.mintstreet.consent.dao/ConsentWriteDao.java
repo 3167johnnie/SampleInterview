5c9b402dec36e0a2e3774d6c05511b3e4d674414
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-06-25 22:10:04 +0530

    Create ConsentWriteDao.java

---
+++ Changes/Purpose_enquiry/new/com.mintstreet.consent.dao/ConsentWriteDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.consent.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.consent.entity.ConsentWriteLog;
+
+public class ConsentWriteDao extends GenericDao<Integer, ConsentWriteLog>{
+	private static final long serialVersionUID = 404346123313078900L;
+
+}
