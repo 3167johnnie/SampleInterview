88326c2de4eb4a0296de717647714d67bdcbdfec
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-07-07 19:57:23 +0530

    Create ApplicationFormCaseCveDao.java

---
+++ Changes/CVE/Code/SBI/src/com/mintstreet/loan/cveloan/dao/ApplicationFormCaseCveDao.java
@@ -0,0 +1,8 @@
+package com.mintstreet.loan.cveloan.dao;
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.cveloan.entity.ApplicationFormCaseCve;
+
+public class ApplicationFormCaseCveDao  extends GenericDao<Integer, ApplicationFormCaseCve> {
+	private static final long serialVersionUID = 1L;
+}
