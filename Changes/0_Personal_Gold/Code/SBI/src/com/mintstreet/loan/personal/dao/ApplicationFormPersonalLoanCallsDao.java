d032f27b9068a01dbd3ef04504b96747c90397ef
3167johnnie <132010839+3167johnnie@users.noreply.github.com>
2026-07-30 18:39:07 +0530

    Create ApplicationFormPersonalLoanCallsDao.java

---
+++ Changes/0_Personal_Gold/Code/SBI/src/com/mintstreet/loan/personal/dao/ApplicationFormPersonalLoanCallsDao.java
@@ -0,0 +1,9 @@
+package com.mintstreet.loan.personal.dao;
+
+
+import com.mintstreet.common.dao.GenericDao;
+import com.mintstreet.loan.personal.entity.ApplicationFormPersonalLoanCalls;
+
+public class ApplicationFormPersonalLoanCallsDao extends GenericDao<Integer, ApplicationFormPersonalLoanCalls> {
+	private static final long serialVersionUID = -4095972777166139972L;
+}
